3f76ebbebb8a5d9b4e73681644ffd811d3c6aa8c
Advertising Avengers <support@advertisingavengers.in>
2026-05-26 20:45:38 +0530

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-B2e2Kgfc.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/SHAIK SHAFI/Documents/advertisingavengers2/synaptix-creative-main/src/routes/__root.tsx", children: ["/"], preloads: ["/advertising-avengers/assets/index-CtOU9WbG.js"] }, "/": { filePath: "C:/Users/SHAIK SHAFI/Documents/advertisingavengers2/synaptix-creative-main/src/routes/index.tsx", children: void 0, preloads: ["/advertising-avengers/assets/index-Gq-NzSWb.js"] } }, clientEntry: "/advertising-avengers/assets/index-CtOU9WbG.js" });
+export {
+  tsrStartManifest
+};
